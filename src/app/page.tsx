a5e1f1e46467dff400727cda4f1a2bbfa1a38c40
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2025-05-09 15:46:52 -0700

    feat(wl-10): Add Slider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,10 @@
-import { Sparkle } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
+import Slider from "@/components/ui/slider";
 
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <Button size="lg" icon>
-          <Sparkle />
-        </Button>
-        <Button icon>
-          <Sparkle />
-        </Button>
-        <Button size="sm" icon>
-          <Sparkle />
-        </Button>
-        <Button size="xs" icon>
-          <Sparkle />
-        </Button>
+        <Slider max={100} step={1} defaultValue={[50]} />
       </main>
     </div>
   );
