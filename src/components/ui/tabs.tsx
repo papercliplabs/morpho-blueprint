da4a3888891ebe3bb511c6fa9087b2de798ad60b
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2025-05-10 16:34:52 -0700

    feat(wl-9): Add Tabs and related components

---
+++ src/components/ui/tabs.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import * as TabsPrimitive from "@radix-ui/react-tabs";
+import * as React from "react";
+
+import { cn } from "@/utils/shadcn";
+
+function Tabs({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Root>) {
+  return <TabsPrimitive.Root data-slot="tabs" className={cn("flex flex-col gap-2", className)} {...props} />;
+}
+
+function TabsList({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.List>) {
+  return (
+    <TabsPrimitive.List data-slot="tabs-list" className={cn("bg-muted flex rounded-md p-1", className)} {...props} />
+  );
+}
+
+function TabsTrigger({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Trigger>) {
+  return (
+    <TabsPrimitive.Trigger
+      data-slot="tabs-trigger"
+      className={cn(
+        "data-[state=active]:bg-card data-[state=active]:body-medium-plus body-medium text-foreground flex h-8 flex-grow items-center justify-center rounded-sm bg-transparent px-4 transition data-[state=active]:shadow",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function TabsContent({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Content>) {
+  return <TabsPrimitive.Content data-slot="tabs-content" className={cn("flex-1 outline-none", className)} {...props} />;
+}
+
+export { Tabs, TabsList, TabsTrigger, TabsContent };
