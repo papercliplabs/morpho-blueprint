58bceb76651069b309cd8fab493129758c4da582
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2025-05-12 15:44:24 -0700

    Switch to `pnpm` instead of `bun` (#11)
    * Switch to `pnpm` instead of `bun`
    
    * Add missing dep `dotenv`
    
    * Add missing peer deps for `eslint-config-next`
    
    * Pin `pnpm` version to `10.10.0`
    
    * Fix build step

--- src/data/whisk/getExample.ts
+++ src/data/whisk/getExample.ts
@@ -2,6 +2,7 @@ import "server-only";
 import { cache } from "react";
 
 import { graphql } from "@/generated/gql/whisk";
+import { GetVaultQuery } from "@/generated/gql/whisk/graphql";
 
 import { whiskClient } from "./client";
 
@@ -15,6 +16,9 @@ const query = graphql(`
 `);
 
 export const getExample = cache(async () => {
-  const vault = await whiskClient.request(query, { chainId: 1, address: "0xd63070114470f685b75B74D60EEc7c1113d33a3D" });
+  const vault = await whiskClient.request<GetVaultQuery>(query, {
+    chainId: 1,
+    address: "0xd63070114470f685b75B74D60EEc7c1113d33a3D",
+  });
   return vault.morphoVault ?? null;
 });
