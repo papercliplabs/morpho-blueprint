8b7fa5b47d7edd4f69b3eb96c24df704d8894610
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2025-05-15 12:59:08 -0700

    [WL-26] Add DialogDrawer component (#24)
    * feat(wl-26): Add Drawer component
    
    * feat(wl-26): Add DialogDrawer component
    
    * fix(wl-26): Remove `hideClose` prop
    
    No longer a prop available on `DialogContent`
    
    * fix(wl-26): Fix entrance/exit animations for dialog

---
+++ src/components/ui/dialog-drawer/index.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { HTMLAttributes, ReactNode, useMemo, useState } from "react";
+import { createContext, useContext } from "react";
+
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Drawer, DrawerContent, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
+import { useResponsiveContext } from "@/providers/ResponsiveProvider";
+
+const DialogDrawerContext = createContext<{ dismissible?: boolean }>({});
+
+// Context to synchronize to ensure they all use the same media query
+export const useDialogDrawerContext = () => {
+  const context = useContext(DialogDrawerContext);
+  if (context === undefined) {
+    throw new Error("useDialogDrawerContext must be used within a DialogDrawer");
+  }
+  return context;
+};
+
+interface DialogDrawerProps {
+  children: ReactNode;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+  dismissible?: boolean; // Via interact outside, or drag (for drawer)
+}
+
+export function DialogDrawer({ open, onOpenChange, dismissible, children }: DialogDrawerProps) {
+  // Syncronize state, allow for controlled or "uncontrolled" use
+  const [openUncontrolled, setOpenUncontrolled] = useState<boolean>(false);
+  const { openInternal, setOpenInternal } = useMemo(() => {
+    return {
+      openInternal: open ?? openUncontrolled,
+      setOpenInternal: onOpenChange ?? setOpenUncontrolled,
+    };
+  }, [open, onOpenChange, openUncontrolled, setOpenUncontrolled]);
+
+  const { isDesktop } = useResponsiveContext();
+  return (
+    <DialogDrawerContext.Provider value={{ dismissible }}>
+      {isDesktop ? (
+        <Dialog open={openInternal} onOpenChange={setOpenInternal}>
+          {children}
+        </Dialog>
+      ) : (
+        <Drawer open={openInternal} onOpenChange={setOpenInternal} dismissible={dismissible}>
+          {children}
+        </Drawer>
+      )}
+    </DialogDrawerContext.Provider>
+  );
+}
+
+export function DialogDrawerTrigger(props: HTMLAttributes<HTMLButtonElement>) {
+  const { isDesktop } = useResponsiveContext();
+  return isDesktop ? <DialogTrigger {...props} /> : <DrawerTrigger {...props} />;
+}
+
+export function DialogDrawerContent({
+  overrideDismissible, // kinda hacky...
+  ...props
+}: { hideCloseButton?: boolean; overrideDismissible?: boolean } & HTMLAttributes<HTMLDivElement>) {
+  const { isDesktop } = useResponsiveContext();
+  const { dismissible } = useDialogDrawerContext();
+  return isDesktop ? (
+    <DialogContent
+      onInteractOutside={(event) => (dismissible || overrideDismissible ? event.preventDefault() : {})}
+      {...props}
+    />
+  ) : (
+    <DrawerContent {...props} />
+  );
+}
+
+export function DialogDrawerTitle(props: HTMLAttributes<HTMLHeadingElement>) {
+  const { isDesktop } = useResponsiveContext();
+  return isDesktop ? <DialogTitle {...props} /> : <DrawerTitle {...props} />;
+}
