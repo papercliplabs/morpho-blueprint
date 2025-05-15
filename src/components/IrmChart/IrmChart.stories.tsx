b4f31613dec6a12562c8811ebea7f32ec0980354
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2025-05-14 22:41:39 -0700

    feat(wl-16): Add IrmChart component

---
+++ src/components/IrmChart/IrmChart.stories.tsx
@@ -0,0 +1,53 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import IrmChart from "@/components/IrmChart";
+
+const meta = {
+  title: "Components/IrmChart",
+  component: IrmChart,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  args: {
+    currentUtilization: 0.2,
+    data: [
+      {
+        utilization: 0.0,
+        supplyApy: 0.0,
+        borrowApy: 0.0012,
+      },
+      {
+        utilization: 0.1,
+        supplyApy: 0.0003,
+        borrowApy: 0.0033,
+      },
+      {
+        utilization: 0.2,
+        supplyApy: 0.0008,
+        borrowApy: 0.0041,
+      },
+      {
+        utilization: 0.3,
+        supplyApy: 0.015,
+        borrowApy: 0.049,
+      },
+      {
+        utilization: 0.4,
+        supplyApy: 0.13,
+        borrowApy: 0.16,
+      },
+      {
+        utilization: 0.5,
+        supplyApy: 0.17,
+        borrowApy: 0.18,
+      },
+    ],
+  },
+} satisfies Meta<typeof IrmChart>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
