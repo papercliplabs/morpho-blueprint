ed0061d55fade94e00a66f4c591d570c32e8d688
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2025-05-15 12:27:09 -0700

    feat(wl-28): Add Step component

---
+++ src/components/Steps/Step.stories.tsx
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Avatar } from "../ui/avatar";
+
+import { Step } from "./Step";
+
+const meta = {
+  title: "Components/Steps/Step",
+  component: Step,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  args: {
+    number: 1,
+    status: "upcoming",
+    label: "Description",
+  },
+  argTypes: {
+    status: {
+      control: { type: "select" },
+      options: ["upcoming", "active", "pending", "complete"],
+    },
+  },
+} satisfies Meta<typeof Step>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Active: Story = {
+  args: {
+    status: "active",
+  },
+};
+
+export const Pending: Story = {
+  args: {
+    status: "pending",
+  },
+};
+
+export const Complete: Story = {
+  args: {
+    status: "complete",
+  },
+};
+
+export const CustomIcon: Story = {
+  args: {
+    icon: <Avatar fallback="CN" src="https://github.com/shadcn.png" size="sm" />,
+  },
+};
