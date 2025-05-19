0517240e8d066bd56379b0f4545c46ef88614b32
spennyp <hello@spencerperkins.me>
2025-05-19 11:01:44 -0700

    Add vault table filtering

--- src/hooks/useVaultTableData.ts
+++ src/hooks/useVaultTableData.ts
@@ -2,9 +2,11 @@
 import { useMemo } from "react";
 import { getAddress } from "viem";
 
+import { FilterKey } from "@/components/filters/types";
 import { VaultPosition } from "@/data/whisk/getVaultPositions";
 import { VaultSummary } from "@/data/whisk/getVaultSummaries";
 
+import { useShallowSearchParams } from "./useShallowSearchParams";
 import { useVaultPositions } from "./useVaultPositions";
 
 export interface VaultTableDataEntry {
@@ -19,6 +21,12 @@ export function useVaultTableData({ vaultSummaries }: { vaultSummaries: VaultSum
 } {
   const { data: positions, isLoading } = useVaultPositions();
 
+  const {
+    values: [chainsFilterValue, assetsFilterValue, curatorsFilterValue, accountFilterValues],
+  } = useShallowSearchParams({
+    keys: [FilterKey.Chains, FilterKey.Assets, FilterKey.Curators, FilterKey.Account],
+  });
+
   const data = useMemo(() => {
     const dataEntries: VaultTableDataEntry[] = [];
 
@@ -26,8 +34,41 @@ export function useVaultTableData({ vaultSummaries }: { vaultSummaries: VaultSum
       const position = positions?.[vaultSummary.chain.id]?.[getAddress(vaultSummary.vaultAddress)];
       dataEntries.push({ vaultSummary, position });
     }
+
     return dataEntries;
   }, [vaultSummaries, positions]);
 
-  return { data, isPositionsLoading: isLoading };
+  const filteredData = useMemo(() => {
+    const filteredData = data.filter((dataEntry) => {
+      const chainsFilterMatch =
+        chainsFilterValue.length === 0 || chainsFilterValue.includes(dataEntry.vaultSummary.chain.name.toString());
+      const assetsFilterMatch =
+        assetsFilterValue.length === 0 || assetsFilterValue.includes(dataEntry.vaultSummary.asset.symbol.toString());
+      // TODO: add curator filter
+      const curatorsFilterMatch = true;
+      void curatorsFilterValue;
+
+      let accountFilterMatch = true;
+      const accountFilterValue = accountFilterValues[0];
+      if (dataEntry.position != undefined && accountFilterValue) {
+        switch (accountFilterValue) {
+          case "positions":
+            accountFilterMatch = BigInt(dataEntry.position.supplyAssets) > 0n;
+            break;
+          case "wallet":
+            accountFilterMatch = (dataEntry.position.walletUnderlyingAssetHolding?.balanceUsd ?? 1) > 0;
+            break;
+          default:
+            // Do nothing
+            break;
+        }
+      }
+
+      return chainsFilterMatch && assetsFilterMatch && curatorsFilterMatch && accountFilterMatch;
+    });
+
+    return filteredData;
+  }, [data, chainsFilterValue, assetsFilterValue, curatorsFilterValue, accountFilterValues]);
+
+  return { data: filteredData, isPositionsLoading: isLoading };
 }
