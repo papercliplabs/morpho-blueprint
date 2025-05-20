4f2358c849120e3165303433dda00a2cf3615dc1
spennyp <hello@spencerperkins.me>
2025-05-19 19:08:03 -0700

    Added vault page market allocation table

---
+++ src/components/tables/MarketAllocationTable.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Table } from "@/components/ui/table";
+import { Vault } from "@/data/whisk/getVault";
+
+import { MarketIdentifier } from "../market/MarketIdentifier";
+import { ApyTooltip } from "../Tooltips/ApyToolip";
+import { TotalSupplyTooltip } from "../Tooltips/TotalSupplyTooltip";
+import NumberFlow from "../ui/number-flow";
+
+interface MarketAllocationTableProps {
+  vault: Vault;
+}
+
+const columns: ColumnDef<Vault["marketAllocations"][number]>[] = [
+  {
+    id: "market",
+    accessorFn: (row) => row.market.name,
+    header: "Market Name",
+    cell: ({ row }) => {
+      const { market } = row.original;
+      return <MarketIdentifier variant="sm" {...market} />;
+    },
+    minSize: 260,
+  },
+  {
+    id: "allocation",
+    accessorKey: "vaultSupplyShare",
+    header: "Allocation",
+    cell: ({ row }) => {
+      const { vaultSupplyShare } = row.original;
+      return <NumberFlow value={vaultSupplyShare} format={{ style: "percent" }} />;
+    },
+    minSize: 160,
+  },
+  {
+    id: "totalSupply",
+    accessorKey: "position.supplyAssetsUsd",
+    header: "Total Supply",
+    cell: ({ row }) => {
+      const { position, supplyCapUsd } = row.original;
+      return <TotalSupplyTooltip totalSupply={position.supplyAssetsUsd} supplyCap={supplyCapUsd} iconPosition="left" />;
+    },
+    minSize: 160,
+  },
+  {
+    id: "supplyApy",
+    accessorKey: "market.supplyApy.total",
+    header: "Total Supply",
+    cell: ({ row }) => {
+      const { market } = row.original;
+      return (
+        <ApyTooltip
+          type="earn"
+          nativeApy={market.supplyApy.base}
+          totalApy={market.supplyApy.total}
+          triggerVariant="sm"
+        />
+      );
+    },
+    minSize: 160,
+  },
+];
+
+export function MarketAllocationTable({ vault }: MarketAllocationTableProps) {
+  return (
+    <Table
+      columns={columns}
+      data={vault.marketAllocations}
+      initialSort={[{ id: "allocation", desc: true }]}
+      rowAction={(row) =>
+        row.market.isIdle
+          ? null
+          : {
+              type: "link",
+              href: `/borrow/${vault.chain.id}/${row.market.marketId}`,
+            }
+      }
+    />
+  );
+}
