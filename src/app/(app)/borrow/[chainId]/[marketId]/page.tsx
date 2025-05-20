532a967f719a4a476fc2dc45dfcf496720728be7
spennyp <hello@spencerperkins.me>
2025-05-19 22:44:19 -0700

    Added market page header

--- src/app/(app)/borrow/[chainId]/[marketId]/page.tsx
+++ src/app/(app)/borrow/[chainId]/[marketId]/page.tsx
@@ -3,6 +3,11 @@ import { Suspense } from "react";
 import { isHex } from "viem";
 
 import MarketActions from "@/components/market/MarketActions";
+import { MarketIdentifier as MarketIdentifierComponent } from "@/components/market/MarketIdentifier";
+import { MarketPositionHighlight } from "@/components/market/MarketPositionHighlight";
+import { BreakcrumbBack } from "@/components/ui/breakcrumb-back";
+import { Card, CardHeader } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 import { WHITELISTED_MARKETS } from "@/config";
 import { getMarket, isNonIdleMarket } from "@/data/whisk/getMarket";
 import { MarketIdentifier } from "@/utils/types";
@@ -25,11 +30,67 @@ export default async function MarketPage({ params }: { params: Promise<{ chainId
   }
 
   return (
-    <div className="flex flex-col">
-      <h1>Market</h1>
-      <Suspense fallback={<div>Loading...</div>}>
-        <MarketActionsWrapper chainId={chainId} marketId={marketId} />
-      </Suspense>
+    <div className="flex w-full min-w-0 flex-col gap-6">
+      <section className="flex flex-col gap-4">
+        <BreakcrumbBack label="Borrow" href="/borrow" />
+        <Suspense
+          fallback={
+            <div className="flex flex-col">
+              <div className="flex h-[64px] items-center gap-3">
+                <Skeleton className="size-8 rounded-full" />
+                <Skeleton className="h-[32px] w-[280px]" />
+              </div>
+              <Skeleton className="h-[20px] w-[140px]" />
+            </div>
+          }
+        >
+          <MarketHeader chainId={chainId} marketId={marketId} />
+        </Suspense>
+      </section>
+
+      <div className="flex flex-col gap-6 lg:flex-row">
+        <div className="flex grow flex-col gap-6">
+          <Card>
+            <CardHeader>Key Metrics</CardHeader>
+            {/* <Suspense fallback={<VaultKeyMetricsSkeleton />}>
+              <KeyMetricsWrapper chainId={chainId} vaultAddress={vaultAddress} />
+            </Suspense> */}
+          </Card>
+
+          <Card>
+            <CardHeader>Vault Allocation</CardHeader>
+            <Suspense fallback={<Skeleton className="h-[400px] w-full" />}>
+              TODO
+              {/* <MarketAllocationTableWrapper chainId={chainId} vaultAddress={vaultAddress} /> */}
+            </Suspense>
+          </Card>
+
+          <Card>
+            <CardHeader>Interest Rate Model</CardHeader>
+            <Suspense fallback={<Skeleton className="h-[400px] w-full" />}>
+              TODO
+              {/* <MarketAllocationTableWrapper chainId={chainId} vaultAddress={vaultAddress} /> */}
+            </Suspense>
+          </Card>
+
+          <Card>
+            <CardHeader>Market Info</CardHeader>
+            {/* <Suspense fallback={<VaultInfoSkeleton />}>
+              <VaultInfoWrapper chainId={chainId} vaultAddress={vaultAddress} />
+            </Suspense> */}
+          </Card>
+        </div>
+
+        <Suspense
+          fallback={
+            <Card className="hidden h-[415px] w-full shrink-0 md:w-[364px] lg:block">
+              <Skeleton className="h-full w-full" />
+            </Card>
+          }
+        >
+          <MarketActionsWrapper chainId={chainId} marketId={marketId} />
+        </Suspense>
+      </div>
     </div>
   );
 }
@@ -43,6 +104,27 @@ function UnsupportedMarket() {
   );
 }
 
+async function MarketHeader({ chainId, marketId }: MarketIdentifier) {
+  const market = await getMarket(chainId, marketId);
+
+  if (!market) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col justify-between gap-4 md:flex-row">
+      <div className="flex flex-col">
+        <div className="flex h-[64px] items-center gap-3">
+          <MarketIdentifierComponent variant="default" {...market} />
+        </div>
+        <div className="text-muted-foreground">{market.chain.name}</div>
+      </div>
+
+      <MarketPositionHighlight market={market} />
+    </div>
+  );
+}
+
 async function MarketActionsWrapper({ chainId, marketId }: MarketIdentifier) {
   const market = await getMarket(chainId, marketId);
 
@@ -52,13 +134,3 @@ async function MarketActionsWrapper({ chainId, marketId }: MarketIdentifier) {
 
   return <MarketActions market={market} />;
 }
-
-// async function ExampleWrapper({ chainId, marketId }: MarketIdentifier) {
-//   const vault = await getMarket(chainId, marketId);
-
-//   return (
-//     <div>
-//       <pre>{JSON.stringify(vault, null, 2)}</pre>
-//     </div>
-//   );
-// }
