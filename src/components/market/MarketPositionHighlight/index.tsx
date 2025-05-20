1768465d2b81155469afbf90c6e5f26953af610b
spennyp <hello@spencerperkins.me>
2025-05-19 22:44:19 -0700

    Added market page header

--- src/components/market/MarketPositionHighlight/index.tsx
+++ src/components/market/MarketPositionHighlight/index.tsx
@@ -1,21 +1,21 @@
 "use client";
 import Image from "next/image";
-import { getAddress } from "viem";
+import { Hex } from "viem";
 import { useAccount } from "wagmi";
 
 import { MetricWithTooltip } from "@/components/Metric";
 import { NumberFlowWithLoading } from "@/components/ui/number-flow";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Vault } from "@/data/whisk/getVault";
-import { useVaultPosition } from "@/hooks/useVaultPositions";
+import { Market } from "@/data/whisk/getMarket";
+import { useMarketPosition } from "@/hooks/useMarketPositions";
 import { descaleBigIntToNumber } from "@/utils/format";
 
-interface VaultPositionHighlightProps {
-  vault: Vault;
+interface MarketPositionHighlightProps {
+  market: Market;
 }
 
-export function VaultPositionHighlight({ vault }: VaultPositionHighlightProps) {
-  const { data, isLoading } = useVaultPosition(vault.chain.id, getAddress(vault.vaultAddress));
+export function MarketPositionHighlight({ market }: MarketPositionHighlightProps) {
+  const { data, isLoading } = useMarketPosition(market.chain.id, market.marketId as Hex);
   const { address } = useAccount();
 
   if (!address) {
@@ -24,19 +24,25 @@ export function VaultPositionHighlight({ vault }: VaultPositionHighlightProps) {
   }
 
   return (
-    <MetricWithTooltip label="Supplying" className="md:items-end" tooltip="Your supply balance in this vault.">
+    <MetricWithTooltip label="Borrowing" className="md:items-end" tooltip="Your borrow balance in this market.">
       <div className="flex flex-col md:items-end">
         <NumberFlowWithLoading
-          value={data?.supplyAssetsUsd ?? undefined}
+          value={data?.borrowAssetsUsd ?? undefined}
           loadingContent={<Skeleton className="mb-1 h-[25px] w-[60px]" />}
           isLoading={isLoading}
           format={{ currency: "USD" }}
           className="heading-5"
         />
         <div className="flex items-center gap-1">
-          <Image src={vault.asset.icon} alt={vault.asset.symbol} width={12} height={12} className="size-3 shrink-0" />
+          <Image
+            src={market.loanAsset.icon}
+            alt={market.loanAsset.symbol}
+            width={12}
+            height={12}
+            className="size-3 shrink-0"
+          />
           <NumberFlowWithLoading
-            value={data?.supplyAssets ? descaleBigIntToNumber(data.supplyAssets, vault.asset.decimals) : undefined}
+            value={data?.borrowAssets ? descaleBigIntToNumber(data.borrowAssets, market.loanAsset.decimals) : undefined}
             loadingContent={<Skeleton className="h-[15px] w-[40px]" />}
             isLoading={isLoading}
             className="body-small-plus text-muted-foreground"
