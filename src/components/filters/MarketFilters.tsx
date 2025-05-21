8be2253fc2f3193e3a9965c074a59ffb6eca6689
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2025-05-20 18:53:12 -0700

    fix(wl-87): Add overflow scroll to filters bar

--- src/components/filters/MarketFilters.tsx
+++ src/components/filters/MarketFilters.tsx
@@ -33,7 +33,7 @@ export function MarketFilters({ chainOptions, collateralAssetOptions, loanAssetO
   );
 
   return (
-    <div className="flex gap-4">
+    <div className="flex gap-4 overflow-x-auto">
       {chainOptions.length > 1 && (
         <MultiSelect
           emptyValue={chainValues.length === 0 ? "All Chains" : "Chains"}
