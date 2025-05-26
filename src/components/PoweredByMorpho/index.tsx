12a9d01cc5ba8d1b55afa11f8a2b2efe0fd2e712
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2025-05-26 16:04:31 -0700

    feat(wl-129): Update usage of PoweredByMorpho

--- src/components/PoweredByMorpho/index.tsx
+++ src/components/PoweredByMorpho/index.tsx
@@ -1,17 +1,18 @@
+"use client";
 import { useTheme } from "next-themes";
 import "./snippet.ts";
 
 type PoweredByMorphoProps = {
   placement?: "left" | "right" | "center";
-};
+} & React.ComponentProps<"div">;
 
-function PoweredByMorpho({ placement }: PoweredByMorphoProps) {
+function PoweredByMorpho({ className, placement }: PoweredByMorphoProps) {
   const { resolvedTheme } = useTheme();
   return (
-    <>
+    <div className={className}>
       {/* @ts-expect-error Property 'powered-by-morpho' does not exist on type 'JSX.IntrinsicElements' */}
       <powered-by-morpho theme={resolvedTheme} placement={placement}></powered-by-morpho>
-    </>
+    </div>
   );
 }
 
