2fa15dec8d9b28448adf5b66cf287d7c14610450
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2025-05-26 17:19:48 -0700

    feat(wl-129): Set props via ref

--- src/components/PoweredByMorpho/index.tsx
+++ src/components/PoweredByMorpho/index.tsx
@@ -1,18 +1,32 @@
 "use client";
 import { useTheme } from "next-themes";
 import "./snippet.ts";
+import { useEffect, useRef } from "react";
 
 type PoweredByMorphoProps = {
   placement?: "center" | "bottom-left" | "bottom-right" | "top-left" | "top-right";
 } & React.ComponentProps<"div">;
 
 function PoweredByMorpho({ className, placement }: PoweredByMorphoProps) {
+  const ref = useRef<HTMLElement>(null);
   const { resolvedTheme } = useTheme();
+
+  useEffect(() => {
+    if (resolvedTheme) {
+      ref.current?.setAttribute("theme", resolvedTheme);
+    }
+  }, [resolvedTheme]);
+
+  useEffect(() => {
+    if (placement) {
+      ref.current?.setAttribute("placement", placement);
+    }
+  }, [placement]);
 
   return (
     <div className={className}>
       {/* @ts-expect-error Property 'powered-by-morpho' does not exist on type 'JSX.IntrinsicElements' */}
-      <powered-by-morpho theme={resolvedTheme} placement={placement}></powered-by-morpho>
+      <powered-by-morpho ref={ref}></powered-by-morpho>
     </div>
   );
 }
