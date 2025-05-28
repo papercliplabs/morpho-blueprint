e0078da5124e347b1206f0bd5d525868b4b94075
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2025-05-28 14:38:24 -0700

    feat(wl-138): Update PoweredByMorpho to custom implementation

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -18,7 +18,7 @@ export function Footer() {
     <footer className="text-muted-foreground mb-18 flex w-full items-start justify-between gap-6 py-6 md:items-center lg:mb-0">
       <div className="flex min-h-9 flex-col gap-6 md:flex-row">
         <LinkExternal href="https://morpho.org/" keepReferrer className="text-muted-foreground">
-          <PoweredByMorpho />
+          <PoweredByMorpho showTooltip={false} />
         </LinkExternal>
         {FOOTER_ITEMS.map((item) => (
           <LinkExternal key={item.name} href={item.href} className="text-muted-foreground">
