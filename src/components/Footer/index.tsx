f6e0cb0292582d5e7d97810129ece2f640ef7c66
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2025-05-31 12:15:04 -0700

    feat(wl-162): Update footer PoweredByMorpho (#101)

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -17,7 +17,7 @@ export function Footer() {
   return (
     <footer className="text-muted-foreground mb-18 flex w-full items-start justify-between gap-6 py-6 md:items-center lg:mb-0">
       <div className="flex min-h-9 flex-col gap-6 md:flex-row">
-        <LinkExternal href="https://morpho.org/" keepReferrer className="text-muted-foreground">
+        <LinkExternal href="https://morpho.org/" keepReferrer className="text-foreground">
           <PoweredByMorpho showTooltip={false} />
         </LinkExternal>
         {FOOTER_ITEMS.map((item) => (
