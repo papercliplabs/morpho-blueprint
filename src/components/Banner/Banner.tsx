94e975dbf5129ea3d10aa00099e5ae4cf5adfdcd
Riderway <riderway@runbox.com>
2025-07-11 21:55:10 +0200

    Fix file formatting

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
-import { type AppConfig } from "@/config/types";
+import type { AppConfig } from "@/config/types";
 import { cn } from "@/utils/shadcn";
 
 interface BannerProps {
@@ -14,17 +14,19 @@ export function Banner(props: BannerProps) {
   const hasButton = button !== undefined;
 
   return (
-    <div className="bg-primary w-full">
-      <div className={cn("text-primary-foreground flex items-center py-2 px-4 max-w-[1182px] mx-auto", {
-      'justify-between gap-4': hasButton,
-      'justify-center': !hasButton,
-    })}>
-      <p className="body-medium line-clamp-1">{text}</p>
-      {hasButton && (
-        <Link href={button.href}>
-          <Button variant="secondary">{button.text}</Button>
-        </Link>
-      )}
+    <div className="w-full bg-primary">
+      <div
+        className={cn("mx-auto flex max-w-[1182px] items-center px-4 py-2 text-primary-foreground", {
+          "justify-between gap-4": hasButton,
+          "justify-center": !hasButton,
+        })}
+      >
+        <p className="body-medium line-clamp-1">{text}</p>
+        {hasButton && (
+          <Link href={button.href}>
+            <Button variant="secondary">{button.text}</Button>
+          </Link>
+        )}
       </div>
     </div>
   );
