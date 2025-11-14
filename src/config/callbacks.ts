a51d6884021624de74ceb9288c82ce256fb00b39
Spencer Perkins <31705429+spennyp@users.noreply.github.com>
2025-11-13 17:53:31 -0800

    feat: add vercel analytics event to eventCb as example (#158)

--- src/config/callbacks.ts
+++ src/config/callbacks.ts
@@ -1,7 +1,9 @@
+"use server";
+import { track } from "@vercel/analytics/server";
 import type { EventName } from "@/data/trackEvent";
 
 export async function eventCb(name: EventName, payload: Record<string, string | number>) {
   // Called from the server whenever an app event occurs.
   // You can send this to the analytics collection pipeline of your choice.
-  console.log("New event received: ", name, payload);
+  await track(name, payload);
 }
